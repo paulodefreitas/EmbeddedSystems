4cd55e033cb2b9692068dde2406fed7d0464d496
DESKTOP-I0CFH6R\paulo <paulo.c.p.de.freitas@gmail.com>
2019-05-27 13:38:11 -0300

    Morse Code - Sounder

---
+++ sketch_may27aMorseCodeSounder/sketch_may27aMorseCodeSounder.ino
@@ -0,0 +1,26 @@
+int buzzerPin = 9;
+int buttonPin = 7;
+int ledPin = 6;
+const int toneFreq = 523;
+
+void setup()
+{
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop()
+{
+  int buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW)
+  {
+    digitalWrite(ledPin, HIGH);
+    tone(buzzerPin, toneFreq);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+    noTone(buzzerPin);
+  }
+}
